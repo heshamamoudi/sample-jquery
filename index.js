c45b307296e7b1a7707b81eb63a86010c8235c67
heshamamoudi <hesham69mez@gmail.com>
2022-04-12 12:38:56 +0300

    jquery test

---
+++ index.js
@@ -0,0 +1,13 @@
+$('#menu').click(function () { 
+    $('#sideNav').addClass('showSide');
+    $('#sideNavbar').toggle();
+
+});
+
+$('#link1,#link2,#link3,#link4').click( ()  => {$('#sideNavbar').hide()
+
+});
+    
+$('#close').click( ()  =>{$('#sideNavbar').hide()
+
+} );
